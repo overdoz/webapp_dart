747981351e6c02266d1d4d1ffd95701aee70eb1a
Thanh <thanh-le@hotmail.de>
2019-02-09 22:25:15 +0100

    double hit logic

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -68,6 +68,14 @@ export default {
       })
     },
     shoot: function (value) {
+        let tempValue;
+        if (doubleHit == true) {
+            tempValue = value * 2;
+            this.doubleHit = false;
+        } else if (tripleHit == true) {
+            tempValue = value * 3;
+            this.tripleHit = false;
+        }
         // Wenn der Wurf <,>,= der aktuellen Punktzahl (- bereits geworfener Würfe)
         switch (true) {
             case value < (this.player[this.currentIndex].score -= this.shots.reduce(this.add,0)):
@@ -78,17 +86,11 @@ export default {
             case value > (this.player[this.currentIndex].score -= this.shots.reduce(this.add,0)):
                 // Wenn der Wurf höher als die Restpunktzahl ist, wechsle zum nächsten Spieler
                 this.shots = [];
-                if (this.currentIndex < this.player.length && this.currentIndex != this.player.length - 1) {
-                    this.currentIndex++;
-                    // der nächste Spieler ist dran
-                    this.currentPlayer = this.player[this.currentIndex];
-                } else {
-                    // Wenn der letzte dran ist, setzte den Index wieder auf 0
-                    this.currentIndex = 0;
-                    this.currentPlayer = this.player[0];
-                }
+                this.nextPlayer();
 
             case value == (this.player[this.currentIndex].score -= this.shots.reduce(this.add,0)):
+                // TODO
+                // doubleHit muss true bleiben, um doubleOut zu ermöglichen
 
         }
 
@@ -96,29 +98,26 @@ export default {
             this.shots.push(value);
         }
 
-        
-        
-
         if (this.shots.length == 3) {
             // füge die Punkte zum Spieler hinzu, berechne den Durchschitt und leere wieder alles
             this.shots = [];
             this.player[this.currentIndex].round++;
             this.player[this.currentIndex].average = this.player[this.currentIndex].points / this.player[this.currentIndex].round
-
-            // wenn der letzte dran ist, index wieder auf 0 setzen
-            if (this.currentIndex < this.player.length && this.currentIndex != this.player.length - 1) {
-                this.currentIndex++;
-                // der nächste Spieler ist dran
-                this.currentPlayer = this.player[this.currentIndex];
-            } else {
-                // Wenn der letzte dran ist, setzte den Index wieder auf 0
-                this.currentIndex = 0;
-                this.currentPlayer = this.player[0];
-            }
+            this.nextPlayer();
+            
         }
     },
     nextPlayer: function () {
-        // TODO
+        // wenn der letzte dran ist, index wieder auf 0 setzen
+        if (this.currentIndex < this.player.length && this.currentIndex != this.player.length - 1) {
+            this.currentIndex++;
+            // der nächste Spieler ist dran
+            this.currentPlayer = this.player[this.currentIndex];
+        } else {
+            // Wenn der letzte dran ist, setzte den Index wieder auf 0
+            this.currentIndex = 0;
+            this.currentPlayer = this.player[0];
+        }
     },
     isGameOver: function () {
         this.player.forEach((child) => {
