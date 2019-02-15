3832d698598bdef4f82329aa655a733141deeb6d
Thanh <thanh-le@hotmail.de>
2019-02-15 23:38:57 +0100

    welcome route

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Config from './views/Config.vue'
 import Game from './views/Game.vue'
+import Welcome from './views/Welcome.vue'
 
 Vue.use(Router)
 
@@ -9,6 +10,11 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    {
+      path: '/',
+      name: 'welcome',
+      component: Welcome
+    },
     {
       path: '/',
       name: 'config',
